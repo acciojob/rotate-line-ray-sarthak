5842d700bb183cb9f57e67387ad222412702350c
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-04 16:40:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+  let angle = 0; // Initial angle in degrees
+
+        function rotateLine() {
+            angle += 2; // Increment angle by 2 degrees
+            document.getElementById("line").style.transform = ` rotate(${angle}deg)`;
+        }
+ setInterval(rotateLine, 20);
+
